c7bbcbb66b390ce89518d19d710977133e0107ec
xiaoLong1010 <chenlong_2014@sina.com>
2014-06-03 22:38:08 +0800

    Initial Commit

---
+++ iOSAppDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOSAppDemo
+//
+//  Created by csy on 6/3/14.
+//  Copyright (c) 2014 cl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CsyAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CsyAppDelegate class]));
+    }
+}
